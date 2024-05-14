0830a8b7ab426e5557293d1ff3a06fc8e0c2896e
Goose Emperor <145610613+goose-emperor@users.noreply.github.com>
2024-05-15 00:14:44 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function button() {
+  document.getElementById("visn").innerHTML =
+    "<br>Автор проекту: Якiв Н. <br> Для проекту використовувались такi ресурси, як: Wikipedia, YouTube, Pinterest, Словник. <br> Фотографiї взятi з вiдкритого ресурсу Pinterest. <br> Допомогало у проектi: додаток для програмування 'Mimo'";
+}
