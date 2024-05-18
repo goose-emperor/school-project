efcdb3b662d6c87af0276b83dd06f83a1ca10fb1
Goose Emperor <145610613+goose-emperor@users.noreply.github.com>
2024-05-19 00:09:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function button() {
   document.getElementById("visn").innerHTML =
-    "<br>Автор проекту: Якiв Н. <br> Для проекту використовувались такi ресурси, як: Wikipedia, YouTube, Pinterest.";
+    "<br>Автор проекту: Якiв Н. <br> Для проекту використовувались такi ресурси, як: <br> Wikipedia, YouTube, Pinterest.";
 }
