c940422fa1e688b8250dcb6720f9b356473f0d4f
Goose Emperor <145610613+goose-emperor@users.noreply.github.com>
2024-05-19 00:02:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function button() {
   document.getElementById("visn").innerHTML =
-    "<br>Автор проекту: Якiв Н. <br> Для проекту використовувались такi ресурси, як: Wikipedia, YouTube, Pinterest, Словник. <br> Фотографiї взятi з вiдкритого ресурсу Pinterest. <br> Допомогало у проектi: додаток для програмування 'Mimo'";
+    "<br>Автор проекту: Якiв Н. <br> Для проекту використовувались такi ресурси, як: Wikipedia, YouTube, Pinterest. <br> Фотографiї взятi з вiдкритого ресурсу Pinterest.;
 }
